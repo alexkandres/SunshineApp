cf62e793317d84c7b93a152511762b1ec7b7e9a0
alexkandres <alexkandres@live.com>
2016-10-23 13:23:06 -0400

    Deleted fragment class and added Detail fragmant inner class to DetailActivity

--- app/src/main/java/com/example/android/sunshineapp/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshineapp/DetailActivity.java
@@ -1,13 +1,14 @@
 package com.example.android.sunshineapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,24 +16,23 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        getMenuInflater().inflate(R.menu.detail, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -45,5 +45,21 @@ public class DetailActivity extends AppCompatActivity {
             return  true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /*
+    Inner fragment instead of .java fragment
+     */
+    public static class PlaceholderFragment  extends Fragment {
+
+        public PlaceholderFragment () {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_detail, container, false);
+        }
     }
 }
